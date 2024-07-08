e829dfe525152a5c8bbebb18ff6e3b69f2091192
Carter K <101891881+Amazn1234@users.noreply.github.com>
2024-07-08 16:12:29 -0700

    Add files via upload
    add finished files for project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//1. Create a new React app.
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../components/App";
+
+//2. Create a App.jsx component.
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//3. Create a Header.jsx component
+
+//4. Create a Footer.jsx component
+
+//5. Create a Note.jsx component
+
+//6. Make sure that the final website is styled like the example shown here:
+//https://l1pp6.csb.app/
